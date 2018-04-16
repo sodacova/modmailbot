59d8b9981e99bb29649177c474763dfdb2bd786f
Brian Tanner <brian@tanner.io>
2018-04-16 06:37:08 -0400

    Add dyno command help (#2)
    * add command
    
    * attempt to fix command
    
    * fix minor bug
    
    * fix missing logModUsername
    
    * add color

---
+++ src/data/commands.js
@@ -0,0 +1,9 @@
+const db = require('../dynodb');
+
+async function getCommand(name) {
+	return await db.models.Command.findOne({ _state: 42, name }).lean();
+}
+
+module.exports = {
+	getCommand,
+};
