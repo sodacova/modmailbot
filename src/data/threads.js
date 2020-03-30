928b598314db5b8616ed711fe970750803db72f8
Kieran <31113295+CoalSephos@users.noreply.github.com>
2020-03-30 20:51:00 +0100

    change formatting of header notes (#11)
    make header notes more human friendly, giving user who created the note and at what time the note was added.

--- src/data/threads.js
+++ src/data/threads.js
@@ -114,7 +114,7 @@ async function createNewThreadForUser(user, quiet = false) {
   let userNotes = await notes.get(user.id)
   if (userNotes && userNotes.length) {
     let note = userNotes.slice(-1)[0];
-    displayNote = `**Note:** ${note.note}\n`;
+    displayNote = `[${note.created_at}] (${note.created_by_name}) => **Note:** ${note.note}\n`;
   } else
     displayNote = '';
   const infoHeader = `NAME **${mainGuildNickname}**\nMENTION ${member ? member.mention : 'UNKNOWN'}\nID **${user.id}**\nACCOUNT AGE **${accountAge}**\n`
