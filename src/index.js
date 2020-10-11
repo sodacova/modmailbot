5aaa6430b96c02a50dd1c24ba4267215282219c3
Bsian <chharry321@gmail.com>
2020-10-11 22:32:17 +0100

    Better stack traces

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ process.on("unhandledRejection", err => {
     if (err.code === 1001 || err.code === 1006) return; // We don't care about connection reset by peer and cloudflare webproxy restart
     // We ignore stack traces for BotErrors (the message has enough info) and network errors from Eris (their stack traces are unreadably long)
     error = new Error(err.message);
+    error.stack = err.stack.split("\n").slice(0, 5).join("\n");
   } else {
     error = err;
   }
