2658ab49927aaab35baa256ee13dc86a1a184ae3
Bsian <chharry321@gmail.com>
2020-10-13 18:31:12 +0100

    Fix migration

--- db/migrations/add_thread_message.js
+++ db/migrations/add_thread_message.js
@@ -1,7 +1,11 @@
 exports.up = async (knex) => {
   await knex.schema.table("thread_messages", (table) => {
-    table.string("thread_message_id", 20).nullable().unique();
+    table.string("thread_message_id", 20).nullable();
   });
 };
 
-exports.down = () => {};
+exports.down = async (knex) => {
+  await knex.schema.table("thread_messages", (table) => {
+    table.dropUnique("thread_message_id");
+  });
+};
