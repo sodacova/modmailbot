c67988113c2e58ed7dd42bf1faada608e66a4700
sodacova <sodacovaa@gmail.com>
2020-11-02 19:59:09 -0500

    Fixed?

--- src/modules/setavatar.js
+++ src/modules/setavatar.js
@@ -1,14 +1,14 @@
 const Eris = require("eris");
 const threadUtils = require("../threadUtils");
 const axios = require("axios");
+const utils = require('../utils')
 
-const validate_img = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/;
+const validate_img = /^http(s)?:\/\/[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+\.(?:png|jpg|gif|webp)$/;
 
 /**
  * @param {Eris.CommandClient} bot
  */
 module.exports = bot => {
-    bot.registerCommandAlias("setav");
     threadUtils.addInboxServerCommand(bot, "setavatar", async (msg, args) => {
 
     let url = args[0];
@@ -19,18 +19,20 @@ module.exports = bot => {
 				headers: { Accept: 'image/*' },
 				responseType: 'arraybuffer'
       });
-      var newav = `data:${response.headers['content-type']};base64,${response.data.toString('base64')}`;
 
-    } catch (err) {
-        console.log(err);
-    }
+      let newav = `data:${response.headers['content-type']};base64,${response.data.toString('base64')}`;
 
-    return bot.editSelf({ avatar: newav })
+      return bot.editSelf({ avatar: newav })
         .then(() => msg.channel.createMessage("<:dynoSuccess:696561641227288639> Successfully changed avatar."))
-        .catch(() => msg.channel.createMessage("<:dynoError:696561633425621078> Couldn\'t change avatar."));
-}, {
+        .catch((err) => utils.handleError(err));
+
+      } catch (err) {
+        new Error(err);
+      }
+    })
+    bot.registerCommandAlias("setav", "setavatar"), {
     requirements: {
-        roleIDs: ["203040224597508096", "523021576128692239"],
+      roleIDs: ["203040224597508096", "523021576128692239"],
     }
-  });
+  }
 };
