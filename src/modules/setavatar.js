2be9cd4e30bacc3e01bfd49977ed2412ad31c39f
sodacova <sodacovaa@gmail.com>
2020-11-10 16:56:53 -0500

    Fix setavatar (again)

--- src/modules/setavatar.js
+++ src/modules/setavatar.js
@@ -1,38 +1,31 @@
 const Eris = require("eris");
 const threadUtils = require("../threadUtils");
 const axios = require("axios");
-const utils = require('../utils')
 
-const validate_img = /^http(s)?:\/\/[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+\.(?:png|jpg|gif|webp)$/;
+const VALIDATE_IMG = /^http(s)?:\/\/[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+\.(?:png|jpg|gif|webp)$/;
 
 /**
  * @param {Eris.CommandClient} bot
  */
 module.exports = bot => {
     threadUtils.addInboxServerCommand(bot, "setavatar", async (msg, args) => {
-
     let url = args[0];
-
-    if (! validate_img.test(url)) return msg.channel.createMessage("<:dynoError:696561633425621078> No image found.");
+    if (! VALIDATE_IMG.test(url)) return msg.channel.createMessage("<:dynoError:696561633425621078> No image found.");
+    let response = await axios.get(url, {
+      headers: { Accept: "image/*" },
+      responseType: "arraybuffer"
+    });
     try {
-			const response = await axios.get(url, {
-				headers: { Accept: 'image/*' },
-				responseType: 'arraybuffer'
-      });
-
-      let newav = `data:${response.headers['content-type']};base64,${response.data.toString('base64')}`;
-
+      const newav = `data:${response.headers["content-type"]};base64,${response.data.toString("base64")}`;
       return bot.editSelf({ avatar: newav })
-        .then(() => msg.channel.createMessage("<:dynoSuccess:696561641227288639> Successfully changed avatar."))
-        .catch((err) => new Error(err));
-
+      .then(() => msg.channel.createMessage("<:dynoSuccess:696561641227288639> Successfully changed avatar."));
       } catch (err) {
-        new Error(err);
+      msg.channel.createMessage(err.response);
       }
-    })
-    bot.registerCommandAlias("setav", "setavatar"), {
+    }), {
     requirements: {
-      roleIDs: ["203040224597508096", "523021576128692239"],
+      custom: (msg) => msg.member.roles.some((r) => ["203040224597508096", "523021576128692239"].includes(r))
     }
-  }
+    };
+  bot.registerCommandAlias("setav", "setavatar");
 };
