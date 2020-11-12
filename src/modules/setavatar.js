9eeef43c6f4a1597dd046a9572409013b36fe921
Bsian <chharry321@gmail.com>
2020-11-12 17:45:54 +0000

    Fix setavatar

--- src/modules/setavatar.js
+++ src/modules/setavatar.js
@@ -1,6 +1,6 @@
 const Eris = require("eris");
 const threadUtils = require("../threadUtils");
-const axios = require("axios");
+const superagent = require("superagent");
 
 const VALIDATE_IMG = /^http(s)?:\/\/[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+\.(?:png|jpg|gif|webp)$/;
 
@@ -8,24 +8,27 @@ const VALIDATE_IMG = /^http(s)?:\/\/[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*
  * @param {Eris.CommandClient} bot
  */
 module.exports = bot => {
-    threadUtils.addInboxServerCommand(bot, "setavatar", async (msg, args) => {
+  threadUtils.addInboxServerCommand(bot, "setavatar", async (msg, args) => {
     let url = args[0];
-    if (! VALIDATE_IMG.test(url)) return msg.channel.createMessage("<:dynoError:696561633425621078> No image found.");
-    let response = await axios.get(url, {
-      headers: { Accept: "image/*" },
-      responseType: "arraybuffer"
-    });
+    if (! VALIDATE_IMG.test(url)) return msg.channel.createMessage("<:dynoError:696561633425621078> Invalid image URL");
+    let response;
     try {
-      const newav = `data:${response.headers["content-type"]};base64,${response.data.toString("base64")}`;
-      return bot.editSelf({ avatar: newav })
-      .then(() => msg.channel.createMessage("<:dynoSuccess:696561641227288639> Successfully changed avatar."));
-      } catch (err) {
-      msg.channel.createMessage(err.response);
-      }
-    }), {
+      response = await superagent.get(url).accept("image/*").responseType("arraybuffer");
+    } catch (error) {
+      if (error.response) { // Server responded with error
+        return msg.channel.createMessage(`<:dynoError:696561633425621078> Server responded with: ${error.status} ${error.message}\n${error.response.text || ""}`);
+      } // Something happened with request
+      return msg.channel.createMessage("<:dynoError:696561633425621078> Unable to send request: " + error.message);
+    }
+    const newav = `data:${response.headers["content-type"]};base64,${response.data.toString("base64")}`;
+    return bot.editSelf({ avatar: newav }).then(
+      () => msg.channel.createMessage("<:dynoSuccess:696561641227288639> Successfully changed avatar"),
+      (e) => msg.channel.createMessage("<:dynoError:696561633425621078> Unable to change avatar: " + e)
+    );
+  }, {
     requirements: {
       custom: (msg) => msg.member.roles.some((r) => ["203040224597508096", "523021576128692239"].includes(r))
     }
-    };
+  });
   bot.registerCommandAlias("setav", "setavatar");
 };
